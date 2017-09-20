810a3199cee171aae83d23e8faf3a1b506d741ce
Tom White <tom@cloudera.com>
2017-09-20 15:18:17 +0100

    Fixes to get ATLAS working on Impala

--- impala/source_source_daimon.sql
+++ impala/source_source_daimon.sql
@@ -5,7 +5,7 @@ truncate ohdsi.source_daimon;
 -- OHDSI CDM source
 INSERT INTO ohdsi.source( source_id, source_name, source_key, source_connection, source_dialect)
 VALUES (1, 'OHDSI CDM V5 Database', 'OHDSI-CDMV5',
-  'jdbc:impala://bottou01.sjc.cloudera.com:21050', 'impala');
+  'jdbc:impala://192.168.99.100:21050', 'impala');
 
 -- CDM daimon
 INSERT INTO ohdsi.source_daimon( source_daimon_id, source_id, daimon_type, table_qualifier, priority) VALUES (1, 1, 0, 'omop_cdm_parquet', 2);
@@ -14,7 +14,7 @@ INSERT INTO ohdsi.source_daimon( source_daimon_id, source_id, daimon_type, table
 INSERT INTO ohdsi.source_daimon( source_daimon_id, source_id, daimon_type, table_qualifier, priority) VALUES (2, 1, 1, 'omop_cdm_parquet', 2);
 
 -- RESULTS daimon
-INSERT INTO ohdsi.source_daimon( source_daimon_id, source_id, daimon_type, table_qualifier, priority) VALUES (3, 1, 2, 'omop_cdm_parquet', 2);
+INSERT INTO ohdsi.source_daimon( source_daimon_id, source_id, daimon_type, table_qualifier, priority) VALUES (3, 1, 2, 'achilles', 2);
 
 -- EVIDENCE daimon
 INSERT INTO ohdsi.source_daimon( source_daimon_id, source_id, daimon_type, table_qualifier, priority) VALUES (4, 1, 3, 'ohdsi', 2);
